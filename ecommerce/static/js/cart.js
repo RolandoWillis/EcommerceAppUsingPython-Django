7879e451c54b86c0cb174dec83f5d63dbc3cf40a
rolandowillis <rolandowillis118@gmail.com>
2022-03-16 03:57:35 +0500

    Added cart.js

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,3 +1,13 @@
 
 //Test - Add the link to file at the end of main.html. Then open browser and check console from inspect to view helloWorld
 //console.log("Hello World")
+
+var updateButtons = document.getElementsByClassName('update-cart')
+for(var i = 0; i < updateButtons.length; i++){
+    updateButtons[i].addEventListener('click', function(){
+        // 'this' is like self. It represents item that gets clicked on.
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+    })
+}

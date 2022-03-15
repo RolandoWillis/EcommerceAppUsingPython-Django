e03566c6ea1936f0f98b0f7ff760cee8185cac0f
rolandowillis <rolandowillis118@gmail.com>
2022-03-15 17:00:07 +0500

    Rendered Cart and Checkout Dynamic Values

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -51,6 +51,17 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderItems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderItems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderItems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderItems])
+        return total
 
 #     Building Order Item
 
@@ -61,6 +72,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -73,6 +88,8 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
+
+
 
 # Once you've made models, run makemigrations and migrate
 # Then go to admin.py to register models
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -18,11 +18,32 @@ def store(request):
 
 # After updating views, go to template store.html
 
+
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer, complete=False)  # get_or_create() => Queries an obj with certain val, if it doesn't exist, it creates it
+        items = order.orderitem_set.all() # Getting All the orderitems that have a certain order as parent | we can query child obj by setting the parent val & then the child obj with all lowercase values
+    else:
+        # if user is not logged in, the page crashes as there is no values to be retrieved on template so we create a manual dict
+        items = []
+        order = {'get_cart_total': 0,'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer, complete=False)  # get_or_create() => Queries an obj with certain val, if it doesn't exist, it creates it
+        items = order.orderitem_set.all() # Getting All the orderitems that have a certain order as parent | we can query child obj by setting the parent val & then the child obj with all lowercase values
+    else:
+        # if user is not logged in, the page crashes as there is no values to be retrieved on template so we create a manual dict
+        items = []
+        order = {'get_cart_total': 0,'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
